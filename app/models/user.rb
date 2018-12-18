2aa56bdbbac46de69872e4a50f6f86fe895cdaae
Micah Shute <micah.shute@gmail.com>
2018-12-18 00:07:17 -0600

    fixed bug when admin deletes user

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,8 +41,8 @@ class User < ApplicationRecord
     has_many :classrooms, dependent: :destroy
     has_many :sent_messages, :class_name => "Message", :foreign_key => "sender_id", dependent: :destroy
     has_many :recieved_messages, :class_name => "Message", :foreign_key => "reciever_id", dependent: :destroy
-    has_many :following_users, foreign_key: "follower_id"
-    has_many :follower_users, class_name: "FollowingUser", foreign_key: "following_id"
+    has_many :following_users, foreign_key: "follower_id", dependent: :destroy
+    has_many :follower_users, class_name: "FollowingUser", foreign_key: "following_id", dependent: :destroy
     has_many :followers, class_name: "User", through: :follower_users, foreign_key: "follower_id"
     has_many :following, class_name: "User", through: :following_users, foreign_key: "following_id"
     has_many :reactions, dependent: :destroy
